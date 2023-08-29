1a2e92a2cd38201f9dbc0e0db417b1d92bcf1e43
Agustín Moragues <amoragues@provinciamicrocreditos.com>
2023-08-29 15:30:23 -0300

    Se devuelve IActionResult para caso exitoso o erróneo.

--- Carga-LegajoDigital/LoadFunction.cs
+++ Carga-LegajoDigital/LoadFunction.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Threading.Tasks;
+using System.Web.Http;
 using LegajoDigitalApp.Business;
 using LegajoDigitalApp.Model;
 using LegajoDigitalDemoApp.Service;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
@@ -13,7 +16,7 @@ namespace Carga_LegajoDigital
     public class LoadFunction
     {
         [FunctionName("LoadFunction")]
-        public void Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ILogger log)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ILogger log)
         {
             try
             {
@@ -24,11 +27,13 @@ namespace Carga_LegajoDigital
                 BusinessLD.ExecuteProccess(LDService,log);
                 
                 log.LogInformation("Se ejecutó correctamente");
+                return new OkObjectResult("Se ejecutó correctamente");
 
             }
             catch (Exception e)
             {
                 log.LogInformation($"Falló proceso de Legajo Digital: {DateTime.Now}" + " " + e.Message);
+                return new BadRequestObjectResult(" " + e.Message);
             }
            
         }
