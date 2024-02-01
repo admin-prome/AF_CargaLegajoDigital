ab0f6dff5ff54d707a42c97130afe856230d3df7
SantiagoMartinezProme <samartinez@provinciamicrocreditos.com>
2024-02-01 16:04:50 -0300

    -feat Retry UploadFileToBlob

--- CargaLegajoDigital/Helper/StorageHelper.cs
+++ CargaLegajoDigital/Helper/StorageHelper.cs
@@ -20,33 +20,51 @@ namespace CargaLegajoDigital.Helper
 
         public void UploadTableDataToBlob(DataTable records, string blobName)
         {
-            try
-            {
-                Uri connectionString = new Uri(storageConnectionString);
-                BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString, null);
-                BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-                string blobNameTxt = blobName + ".txt";
-                using (MemoryStream stream = new MemoryStream())
-                {
-                    using (StreamWriter writer = new StreamWriter(stream))
-                    {
-                        writer.WriteLine(string.Join(";", records.Columns.Cast<DataColumn>().Select(col => col.ColumnName)));
+            int maxRetries = 3;
+            int retryCount = 0;
+            bool uploadedSuccessfully = false;
 
-                        foreach (DataRow row in records.Rows)
+            while (!uploadedSuccessfully && retryCount < maxRetries)
+            {
+                try
+                {
+                    Uri connectionString = new Uri(storageConnectionString);
+                    BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString, null);
+                    BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+                    string blobNameTxt = blobName + ".txt";
+
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        using (StreamWriter writer = new StreamWriter(stream))
                         {
-                            writer.WriteLine(string.Join(";", row.ItemArray));
+                            writer.WriteLine(string.Join(";", records.Columns.Cast<DataColumn>().Select(col => col.ColumnName)));
+
+                            foreach (DataRow row in records.Rows)
+                            {
+                                writer.WriteLine(string.Join(";", row.ItemArray));
+                            }
+                            writer.Flush();
+                            stream.Position = 0;
+                            containerClient.UploadBlob(blobNameTxt, stream);
+                            BlobHttpHeaders headers = new BlobHttpHeaders { ContentType = "text/plain" };
+                            containerClient.GetBlobClient(blobNameTxt).SetHttpHeaders(headers);
                         }
-                        writer.Flush();
-                        stream.Position = 0;
-                        containerClient.UploadBlob(blobNameTxt, stream);
-                        BlobHttpHeaders headers = new BlobHttpHeaders { ContentType = "text/plain" };
-                        containerClient.GetBlobClient(blobNameTxt).SetHttpHeaders(headers);
+                    }
+
+                    uploadedSuccessfully = true; 
+                }
+                catch (Exception e)
+                {
+                    retryCount++;
+                    if (retryCount < maxRetries)
+                    {
+                        Console.WriteLine($"Error uploading table data to blob (Retry {retryCount}/{maxRetries}): {e.Message}");
+                    }
+                    else
+                    {
+                        throw new Exception($"Error uploading table data to blob after {maxRetries} attempts: {e.Message}");
                     }
                 }
-            }
-            catch (Exception e)
-            {
-                throw new Exception("Error uploading table data to blob: " + e.Message);
             }
         }
     }
